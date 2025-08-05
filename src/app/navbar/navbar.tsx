d9d0dac9b214edf0ffd3a62fb0dbfd48382b285e
Armando Gonzalez <armangonzal@gmail.com>
2025-08-05 17:09:46 -0500

    fix: update Navbar links to point to root path

--- src/app/navbar/navbar.tsx
+++ src/app/navbar/navbar.tsx
@@ -8,9 +8,9 @@ export default function Navbar() {
 
       </div>
       <ul className="flex gap-6">
-        <li><a href="/#home" className="hover:underline">Home</a></li>
-        <li><a href="/#about" className="hover:underline">About</a></li>
-        <li><a href="/#contact" className="hover:underline">Contact</a></li>
+        <li><a href="/" className="hover:underline">Home</a></li>
+        <li><a href="/" className="hover:underline">About</a></li>
+        <li><a href="/" className="hover:underline">Contact</a></li>
       </ul>
     </nav>
   );
