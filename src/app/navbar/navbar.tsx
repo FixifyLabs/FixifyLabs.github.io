5e2c4654b2241040d981b0dbdb186958683e8755
Armando Gonzalez <armangonzal@gmail.com>
2025-08-05 14:34:24 -0500

    feat: update dependencies to use caret (^) versioning and add Navbar component

---
+++ src/app/navbar/navbar.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+export default function Navbar() {
+  return (
+    <nav className="flex items-center justify-between gap-2 p-4 bg-gray-200 text-black">
+      <div className="flex items-center gap-4">
+        <Image src="/static/images/logo.png" alt="fixify LABS" width={200} height={200} />
+
+      </div>
+      <ul className="flex gap-6">
+        <li><a href="/#home" className="hover:underline">Home</a></li>
+        <li><a href="/#about" className="hover:underline">About</a></li>
+        <li><a href="/#contact" className="hover:underline">Contact</a></li>
+      </ul>
+    </nav>
+  );
+}
